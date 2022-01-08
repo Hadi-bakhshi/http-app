949ecbd8712d7f8d873c7d512968d26dca4bad68
Hadi <hadibakhshi277@gmail.com>
2022-01-08 23:29:38 +0330

    newcomment component developed

--- src/Components/NewComment/NewComment.js
+++ src/Components/NewComment/NewComment.js
@@ -1,22 +1,43 @@
-import './newcomment.css'
+import axios from "axios";
+import { useState } from "react";
+import "./newcomment.css";
 const NewComment = () => {
-    return (
-        <div className='ncContainer'>
-            <div>
-                <label>Name</label>
-                <input type="text" />
-            </div>
-            <div>
-                <label>Email</label>
-                <input type="text" />
-            </div>
-            <div>
-                <label>Body</label>
-                <input type="textarea" />
-            </div>
-          
-        </div>
-    )
-}
+  const [comment, setComment] = useState({
+    name: "",
+    email: "",
+    content: "",
+  });
+  const changeHandler = (e) => {
+    setComment({ ...comment, [e.target.name]: e.target.value });
+  };
 
-export default NewComment
+  const postCommentHandler = () => {
+    axios
+      .post("https://jsonplaceholder.typicode.com/comments", {
+        ...comment,
+        postId: 100,
+      })
+      .then((res) => console.log(res.data))
+      .catch((error) => console.log(error));
+  };
+
+  return (
+    <div className="ncContainer">
+      <div>
+        <label>Name</label>
+        <input type="text" onChange={changeHandler} name="name" />
+      </div>
+      <div>
+        <label>Email</label>
+        <input type="text" onChange={changeHandler} name="email"/>
+      </div>
+      <div>
+        <label>Body</label>
+        <textarea type="textarea" onChange={changeHandler} name="content" />
+      </div>
+      <button onClick={postCommentHandler}>Add New Comment</button>
+    </div>
+  );
+};
+
+export default NewComment;
