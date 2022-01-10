4d0a272a231991f19f4cff55ae783d8cf78fceaf
Hadi <hadibakhshi277@gmail.com>
2022-01-10 13:16:35 +0330

    toastify added to handle error

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-
-import './App.css';
-import Discussion from './Container/Discussion/Discussion';
+import "./App.css";
+import Discussion from "./Container/Discussion/Discussion";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   return (
     <div className="App">
-     <Discussion/>
+      <ToastContainer />
+      <Discussion />
     </div>
   );
 }
--- src/Container/Discussion/Discussion.js
+++ src/Container/Discussion/Discussion.js
@@ -4,17 +4,22 @@ import FullComment from "../../Components/FullComment/FullComment";
 import NewComment from "../../Components/NewComment/NewComment";
 import { useEffect, useState } from "react";
 import axios from "axios";
+import { toast } from "react-toastify";
 
 const Discussion = () => {
   const [comments, setComments] = useState(null);
   const [selectedId, setSelectedId] = useState(null);
+  const [error, setError] = useState(false);
+
   useEffect(() => {
     const getComments = async () => {
       try {
         const { data } = await axios.get("http://localhost:3001/comments");
         setComments(data);
       } catch (error) {
-        console.log(error);
+        // console.log(error);
+        setError(true);
+        // you should get the error message from the backend
       }
     };
     getComments();
@@ -35,24 +40,32 @@ const Discussion = () => {
       .catch((error) => console.log(error));
   };
 
- 
+  const renderComments = () => {
+    // first it tries to load the comments from the backend
+    let renderedComments = <p> Loading ...</p>;
+    // then if there are some error show the error message
+    if (error) {
+      renderedComments = <p> fetching data failed </p>;
+      toast.error("There is an error");
+    }
+
+    // if everything is fine then render the comments
+    if (comments && !error) {
+      renderedComments = comments.map((c) => (
+        <Comment
+          key={c.id}
+          name={c.name}
+          email={c.email}
+          onClick={() => selectCommentHandler(c.id)}
+        />
+      ));
+    }
+    return renderedComments;
+  };
 
   return (
     <main>
-      <section>
-        {comments ? (
-          comments.map((c) => (
-            <Comment
-              key={c.id}
-              name={c.name}
-              email={c.email}
-              onClick={() => selectCommentHandler(c.id)}
-            />
-          ))
-        ) : (
-          <p>Loading...</p>
-        )}
-      </section>
+      <section>{renderComments()}</section>
       <section>
         <FullComment setComments={setComments} commentId={selectedId} />
       </section>
